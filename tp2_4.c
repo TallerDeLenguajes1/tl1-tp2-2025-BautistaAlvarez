d8c6ffc8bbb4c981c294464f606003c0e8a66b50
Juan <AlvarezBautista26@hotmail.com>
2025-04-06 17:29:33 -0300

    Commit para la primera funcion

--- tp2_4.c
+++ tp2_4.c
@@ -11,6 +11,7 @@ struct{
    char tipos[6][10] = {"Intel", "AMD", "Celeron", "Athlon", "Core", //usar este tipo de arreglo
     "Pentium"};
     
+    void listarPCs(compu pcs[], int cantidad);
 
 int main(){
     srand(time(NULL));
@@ -27,8 +28,22 @@ int main(){
         pc[i].cantidad_nucleos = 1 + rand()%(8 - 1 + 1);
         pc[i].tipo_cpu = tipos[rand() % 6];
     }
+
+    listarPCs(pc,numCompu);
     
 
 
     return 0;
+}
+
+void listarPCs(compu pcs[], int cantidad){
+    for (int i = 0; i < cantidad; i++)
+    {
+        printf("Pc %d\n", i+1);
+        printf("Anio de fabricacion: %d\n", pcs[i].anio);
+        printf("Cantidad de nucleos: %d\n", pcs[i].cantidad_nucleos);
+        printf("Velocidad de procesamiento en GHz: %d\n", pcs[i].velocidad);
+        printf("Tipo de procesador: %s\n", pcs[i].tipo_cpu);
+    }
+    
 }
